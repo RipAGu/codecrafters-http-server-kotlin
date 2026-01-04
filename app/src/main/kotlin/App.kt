621f27866afafab3387dd755dd5a056bc3f75716
yh.song <dudgus907@naver.com>
2026-01-04 23:46:16 +0900

    codecrafters submit [skip ci]

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,44 +1,42 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.ServerSocket;
+import java.net.ServerSocket
+
+const val OK = "HTTP/1.1 200 OK\r\n"
+const val NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n"
 
 fun main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println("Logs from your program will appear here!")
 
-    var serverSocket = ServerSocket(4221)
+    // Uncomment this block to pass the first stage
+    val serverSocket = ServerSocket(4221)
 
     // Since the tester restarts your program quite often, setting SO_REUSEADDR
     // ensures that we don't run into 'Address already in use' errors
     serverSocket.reuseAddress = true
-    val response200 = "HTTP/1.1 200 OK\r\n"
-    val response404 = "HTTP/1.1 404 Not Found\r\n\r\n"
 
-    val socket = serverSocket.accept() // Wait for connection from client.
-
-    val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
-    val requestLine = reader.readLine()
-    val output = socket.getOutputStream()
-
-    val paths = requestLine.split(" ")
-
-    val path = if (paths.size > 1) paths[1] else "/"
-    val prefix = path.split("/")
-    val isEcho = prefix.size > 2 && prefix[1] == "echo"
-
-    if (path != "/" && !isEcho) {
-        output.write(response404.toByteArray())
-    } else if (path == "/") {
-        val res = response200 + "\r\n"
-        output.write(res.toByteArray())
-    } else {
-        val echo = if (prefix[1] == "echo") prefix[2] else ""
-        val header = "Content-Type: text/plain\r\nContent-Length: ${echo.length}\r\n\r\n${echo}"
-
-        val res = response200 + header
-        output.write(res.toByteArray())
-
-    }
-
+    val socket = serverSocket.accept() // Wait for connection from a client.
     println("accepted new connection")
+
+    val reader = socket.inputStream.bufferedReader()
+    val requestLine = reader.readLine()
+
+    val (method, path, protocol) = requestLine.split(" ")
+
+    when {
+        path == "/" -> socket.outputStream.write((OK + "\r\n").toByteArray())
+        path.startsWith("/echo") -> echo(path, socket)
+        else -> socket.outputStream.write(NOT_FOUND.toByteArray())
+    }
+}
+
+fun echo(path: String, socket: java.net.Socket) {
+    val payload = path.removePrefix("/echo/")
+    val rawResponse = StringBuilder()
+    rawResponse.append(OK)
+    rawResponse.append("Content-Type: text/plain\r\n")
+    rawResponse.append("Content-Length: ${payload.length}\r\n")
+    rawResponse.append("\r\n")
+    rawResponse.append(payload)
+
+    socket.outputStream.write(rawResponse.toString().toByteArray())
 }
